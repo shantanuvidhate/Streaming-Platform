d23e8f4f4b02e8362e74d2ac3aeb2d72e8c3903c
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-14 12:36:50 +0530

    Custom Button

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx";
+
+interface ButtonProps {
+    type?: "button" | "submit" | "reset" | undefined;
+    fullWidth?: boolean;
+    children?: React.ReactNode;
+    onClick?: () => void;
+    secondary?: boolean;
+    danger?: boolean;
+    disabled?: boolean;
+  }
+  const Button: React.FC<ButtonProps> = ({
+    type = "button",
+    fullWidth,
+    children,
+    onClick,
+    secondary,
+    danger,
+    disabled,
+  }) => {
+    return ( 
+      <button
+        onClick={onClick}
+        type={type}
+        disabled={disabled}
+        className={clsx(`
+        bg-red-600 text-white w-full mt-6 hover:bg-red-700 transition py-3 rounded-md font-semibold`,
+        )}
+      >
+        {children}
+      </button>
+     );
+  }
+   
+  export default Button;
