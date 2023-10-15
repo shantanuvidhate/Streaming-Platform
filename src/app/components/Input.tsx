af3f8a7790523d0f8f933447599c8ff9a46c5c70
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-15 07:34:21 +0530

    Input peer issue fix

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { 
-    FieldErrors, 
-    FieldValues, 
-    UseFormRegister 
-  } from "react-hook-form";
+import {
+  FieldErrors,
+  FieldValues,
+  UseFormRegister
+} from "react-hook-form";
 
-interface InputProps{
+interface InputProps {
   label: string;
   id: string;
   type?: string;
@@ -16,19 +16,19 @@ interface InputProps{
   disabled?: boolean;
 }
 const Input: React.FC<InputProps> = ({
-    label,
-    id,
-    register,
-    required,
-    errors,
-    type = 'text',
-    disabled,
-  }) => {
-    return (
-        <div className="relative">
-            <label htmlFor={id} className="absolute text-base text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">{label}</label>
-            <input id={id} type={type} autoComplete={id} disabled={disabled} {...register(id,{required})} className="block rounded-md px-6 pt-6 pb-1 w-full text-base text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer" placeholder="" />
-        </div>
-    );
+  label,
+  id,
+  register,
+  required,
+  errors,
+  type = 'text',
+  disabled,
+}) => {
+  return (
+    <div className="relative">
+      <input className="block rounded-md px-6 pt-6 pb-1 w-full text-base text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer" id={id} type={type} autoComplete={id} disabled={disabled} {...register(id, { required })} placeholder="" />
+      <label className="absolute text-base text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3" htmlFor={id} >{label}</label>
+    </div>
+  );
 }
 export default Input;
