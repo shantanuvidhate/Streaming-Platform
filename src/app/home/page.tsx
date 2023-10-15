73c850cac9d996405031359caa368afd88f2534f
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-15 17:43:10 +0530

    BillBoard Component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,11 @@
+import Billboard from "../components/Billboard";
 import NavBar from "../components/Navbar";
 
-export default function Home () {
+export default async function Home () {
     return (
+        <>
         <NavBar />
+        <Billboard />
+        </>
     );
 }
