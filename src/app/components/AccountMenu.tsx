f5d898f577df97b78cf5022c54223da2b5c75c46
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-15 13:37:38 +0530

    user name from session

--- src/app/components/AccountMenu.tsx
+++ src/app/components/AccountMenu.tsx
@@ -1,10 +1,11 @@
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import React from "react";
 
 interface AccountMenuProps {
     visible?: boolean;
 }
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+    const session = useSession();
     if (!visible) {
         return null;
     }
@@ -13,7 +14,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
             <div className="flex flex-col gap-3">
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/default-blue.png" alt="Profile" />
-                    <p className="text-white text-sm group-hover/item:underline">Username</p>
+                    <p className="text-white text-sm group-hover/item:underline"> {session?.data?.user?.name} </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4" />
                 <div onClick={()=>signOut()} className="px-3 text-center text-white text-sm hover:underline">
