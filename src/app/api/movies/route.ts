282d48774f648efb12d0a554ee7a2404f708cd31
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-16 17:02:53 +0530

    Movie List API

---
+++ src/app/api/movies/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+
+
+export async function GET(request: NextResponse, response: NextResponse) {
+    let success = true;
+    try {
+        const movies = await prismadb.movie.findMany();
+        return NextResponse.json(movies);
+    } catch (error) {
+        success = false;
+        console.log(error + "From movies API");
+        return NextResponse.json({success});
+    }
+}
