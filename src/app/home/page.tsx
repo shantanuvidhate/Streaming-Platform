09d52a019e97abaab0a72b9b7bc584d74bf0def0
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-16 17:01:34 +0530

    getMovieList API Integration

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,29 @@
+"use client"
 import Billboard from "../components/Billboard";
+import MovieList from "../components/MovieList";
 import NavBar from "../components/Navbar";
 
-export default async function Home () {
+const getMovieList = async () => {
+    try {
+        const res = await fetch("http://localhost:3000/api/movies")
+        const data = await res.json();
+        // console.log(data);
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const Home=async()=>{
+    let data = await getMovieList();
     return (
         <>
-        <NavBar />
-        <Billboard />
+            <NavBar />
+            <Billboard />
+            <div className="pb-40">
+                <MovieList title="Trending Now" data={data} />
+            </div>
         </>
     );
 }
+export default Home;
