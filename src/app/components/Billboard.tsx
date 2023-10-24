e0d0ec33389cb133c88075f01b884428630727da
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-10-24 17:19:32 +0530

    transition added

--- src/app/components/Billboard.tsx
+++ src/app/components/Billboard.tsx
@@ -17,7 +17,7 @@ export default function Billboard() {
     }
     return (
         <div className="relative h-[56.25vw]">
-            <video autoPlay muted loop className="w-full h-[56.25vw] object-cover brightness-[60%]" src={data?.videoUrl}> </video>
+            <video autoPlay muted loop className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500" src={data?.videoUrl}> </video>
             <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
                 <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl"> {data?.title} </p>
                 <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl"> {data?.description} </p>
